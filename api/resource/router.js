b000bc774b32a4e65b66ceef50e3af72234e086e
Ryan Ishimaru <rishimaru23@gmail.com>
2021-07-16 11:44:56 -0700

    created db and resources router/model functional

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,31 @@
 // build your `/api/resources` router here
+const router = require('express').Router()
+const Resource = require('./model')
+
+router.get('/', async (req, res, next) => {
+    try {
+        const resources = await Resource.getResources()
+        res.status(200).json(resources)
+    } catch(err) {
+        next(err)
+    }
+})
+
+router.post('/', async (req, res, next) => {
+    const existingName = await Resource.getResourceByName(req.body.resource_name)
+    if(!req.body.resource_name) {
+        next({ status: 400, message: 'missing required resource_name'})
+    }else if(existingName.length === 1) {
+        next({ status: 400, message: 'resource_name must be unique'})
+    } else {
+        const newResource = await Resource.createResource(req.body)
+        try {
+            res.status(201).json(newResource)
+        }
+        catch(err){
+            next(err)
+        }
+    }
+})
+
+module.exports = router 
--- api/server.js
+++ api/server.js
@@ -1,1 +1,17 @@
 // build your server here and require it from index.js
+const express = require('express')
+const resourceRouter = require('./resource/router')
+
+const server = express()
+
+server.use(express.json())
+server.use('/api/resources', resourceRouter)
+
+server.use((err, req, res, next) => { //eslint-disable-line
+    res.status(err.status || 500).json({
+        message: err.message,
+        stack: err.stack
+    })
+})
+
+module.exports = server
